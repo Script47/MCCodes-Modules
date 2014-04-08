60fc5550548cdd5af9203cdbf1bf03252ab55cdb
Script47 <Script47@hotmail.com>
2014-04-08 13:16:26 +0100

    Bug Fix
    With the new line of code, you won't have to click delete twice, it will
    automatically redirect you to users profile once you have pressed delete
    comment.

--- Advanced View User System/viewuser.php
+++ Advanced View User System/viewuser.php
@@ -330,6 +330,7 @@ echo '</table>';
 if(isset($_GET['delete'])) {
     $commentID = htmlspecialchars(trim($_GET['commentID']));
     $db->query("DELETE FROM `comments` WHERE ID=$commentID");
+    exit(header("Location: viewuser.php?u=$profileID"));    
 }
 
 function checkblank($in) {
